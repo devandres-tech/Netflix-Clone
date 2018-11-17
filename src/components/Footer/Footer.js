d457ad851f171cae53d9f1994e9f6cc38e1f1da5
andres alcocer <andresalcocer7@yahoo.com>
2018-11-17 12:58:54 -0500

    main layout completed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,24 +3,8 @@ import React from 'react';
 const footer = () => (
   
    <footer className="footer">
-      <div className="footer__heading">icons</div>
-      <div className="footer__container">
-         <div className="footer__container--description">desc 1</div>
-         <div className="footer__container--description">desc 2</div>
-         <div className="footer__container--description">desc 3</div>
-         <div className="footer__container--description">desc 4</div>
-         <div className="footer__container--description">desc 5</div>
-         <div className="footer__container--description">desc 6</div>
-         <div className="footer__container--description">desc 7</div>
-         <div className="footer__container--description">desc 8</div>
-         <div className="footer__container--description">desc 9</div>
-         <div className="footer__container--description">desc 10</div>
-         <div className="footer__container--description">desc 11</div>
-         <div className="footer__container--description">desc 12</div>
-         <div className="footer__container--description">desc 13</div>
-      </div>
-      <div className="footer__serviceCode">service code</div>
-      <div className="footer__copyright">copyright</div>
+   
+      <div className="footer__copyright">Copyright &copy; 2018 Made with ❤️ by Andres Alcocer</div>
    </footer>
 );
 
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import axios from 'axios'; 
 import Header from '../../components/Header/Header'; 
 import MovieShowcase from '../MovieShowcase/MovieShowcase'; 
-import MovieOriginals from '../MovieOriginals/MovieOriginals'; 
 import Footer from '../../components/Footer/Footer'; 
 import { BrowserRouter } from "react-router-dom"; 
 
