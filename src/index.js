ddc766288c8b4788f6c7c1afc9e0232bb061c10e
andres alcocer <andresalcocer7@yahoo.com>
2020-08-07 14:35:11 -0400

    removed unused imports

--- src/index.js
+++ src/index.js
@@ -8,10 +8,6 @@ import '@babel/polyfill';
 
 import App from './containers/App';
 import 'swiper/swiper-bundle.min.css';
-// import 'swiper/components/navigation/navigation.scss';
-// import 'swiper/components/pagination/pagination.scss';
-// import 'swiper/components/scrollbar/scrollbar.scss';
-// Import main sass file to apply global styles
 import './static/sass/style.scss';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
