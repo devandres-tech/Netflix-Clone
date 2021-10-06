93a1a22efdc5920aec7f73065bd18ac361a2a3ea
andres alcocer <andresalcocer7@yahoo.com>
2021-10-06 11:13:24 -0400

    set prettier rules

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import MainContent from './MainContent';
-import Modal from '../components/UI/Modal';
-import MovieDetails from '../components/Movie/MovieDetails';
+import MainContent from './MainContent'
+import Modal from '../components/UI/Modal'
+import MovieDetails from '../components/Movie/MovieDetails'
 
 class Home extends Component {
   state = {
@@ -10,22 +10,22 @@ class Home extends Component {
     toggleModal: false,
     /** Holds the movie information for a single movie. */
     movieOverview: {},
-  };
+  }
 
   /* Get the appropriate details for a specific movie that was clicked */
   selectMovieHandler = async (movie) => {
-    this.setState({ toggleModal: true });
-    await this.setState({ movieOverview: movie });
-  };
+    this.setState({ toggleModal: true })
+    await this.setState({ movieOverview: movie })
+  }
 
   closeModal = () => {
-    this.setState({ toggleModal: false });
-  };
+    this.setState({ toggleModal: false })
+  }
 
   render() {
     return (
       <>
-        <div className="main-content">
+        <div className='main-content'>
           <MainContent selectMovieHandler={this.selectMovieHandler} />
         </div>
         <Modal
@@ -36,8 +36,8 @@ class Home extends Component {
           <MovieDetails movie={this.state.movieOverview} />
         </Modal>
       </>
-    );
+    )
   }
 }
 
-export default Home;
+export default Home
