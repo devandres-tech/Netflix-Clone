8ee1dc1bfd943aad607da3d704879705e8fabead
andres alcocer <andresalcocer7@yahoo.com>
2021-10-08 20:38:30 -0400

    updated modal styles

--- src/components/UI/Backdrop.js
+++ src/components/UI/Backdrop.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
 const backdrop = ({ toggleBackdrop, show }) =>
-  show ? <div onClick={toggleBackdrop} className='backdrop'></div> : null
+  show ? (
+    <div
+      onClick={toggleBackdrop}
+      className={`backdrop ${show ? 'hideB' : ''}`}
+    ></div>
+  ) : null
 
 export default backdrop
