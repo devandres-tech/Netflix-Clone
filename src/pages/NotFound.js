c03104c8dc51d52b453f64665c4510d1b28d0149
MOHAMED RIAD <mohamedriiad@gmail.com>
2021-12-15 22:40:24 +0100

    Add Netflix not found page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,7 +3,19 @@ import React from 'react';
 const NotFound = () => {
 	return (
 		<div className='not-found__message'>
-			<h1>NOT FOUND</h1>
+			{/* TODO: add css to match netflix not found page */}
+			<h1>LOST YOUR WAY?</h1>
+			<p>
+				Sorry, we can't find that page. You'll find lots to explore on
+				the home page.
+			</p>
+			<button>Netflix Home</button>
+			<span>
+				Error Code <strong>NSES-404</strong>
+			</span>
+			<span>
+				FROM <strong>LOST IN SPACE</strong>
+			</span>
 		</div>
 	);
 };
