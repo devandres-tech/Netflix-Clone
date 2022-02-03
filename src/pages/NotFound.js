c301bcf316eb53ac3d916ae3d0cca9402b214a20
andres alcocer <andresalcocer7@yahoo.com>
2022-02-03 18:58:50 -0500

    formatted code

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
-const  NotFound = () => {
+const NotFound = () => {
   return (
     <div>
       <h1>NOT FOUND</h1>
     </div>
-  );
+  )
 }
 
 export default NotFound
