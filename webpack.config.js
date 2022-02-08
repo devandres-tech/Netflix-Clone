f98eae39fce122cadd09a91946570b4547e52570
andres alcocer <andresalcocer7@yahoo.com>
2022-02-08 18:53:25 -0500

    console.log dotenv 3

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ var path = require('path')
 module.exports = () => {
   // call dotenv and it will return an Object with a parsed key
   const env = dotenv.config().parsed
+  console.log('process------', process.env)
   console.log('env------', dotenv.config())
 
   // reduce env variables to an oject
