6b3d261c6d0e9ac28166b00d52c90b18a5f4345a
Harish Kumar Verma <VERM396533@exactsoftware.com>
2023-04-23 16:29:32 +0530

    Fixed not_found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,21 +2,28 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div className='not-found__message'>
-      {/* TODO: add css to match netflix not found page */}
-      <h1>LOST YOUR WAY?</h1>
-      <p>
-        Sorry, we can't find that page. You'll find lots to explore on the home
-        page.
-      </p>
-      <button>Netflix Home</button>
-      <span>
-        Error Code <strong>NSES-404</strong>
-      </span>
-      <span>
-        FROM <strong>LOST IN SPACE</strong>
-      </span>
+<div className="not-found">
+  <div className="not-found--header">
+    <div className="not-found--logo">
+      <a href="/">Netflix</a></div>
     </div>
+    <div className="not-found--content">
+        <h1>Lost your way?</h1>
+        <div className="not-found--content--body">
+          <p>Sorry, we can't find that page. You'll find lots to explore on the home page.</p>
+          <div className="not-found--content--buttons">
+            <a href="/">
+              <button className="color-primary not-found--button" type="button">
+                <span>Netflix Home</span></button>
+                </a>
+                </div>
+                <div className="not-found--content--errorCode">
+                  <span id="" data-uia="">Error Code <strong>NSES-404</strong></span>
+                </div>
+            </div>
+      </div>
+        <span id="" className="not-found--imageSource" >FROM <strong>LOST IN SPACE</strong></span>
+  </div>
   )
 }
 
